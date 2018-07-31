0f366fa9e8c75b6e41e327e775fe77b091563b20
Taylor R Campbell <riastradh@NetBSD.org>
2018-07-31 19:55:23 +0000

    Fix up drm_crtc.c.
    - Ifdef out debugfs variables.
    - Restore int ret declaration lost in merge.
    - Use PRIu64, not %llu, for uint64_t.

--- sys/external/bsd/drm2/dist/drm/drm_crtc.c
+++ sys/external/bsd/drm2/dist/drm/drm_crtc.c
@@ -1035,7 +1035,9 @@ EXPORT_SYMBOL(drm_connector_index);
  */
 int drm_connector_register(struct drm_connector *connector)
 {
+#ifndef __NetBSD__		/* XXX sysfs, debugfs */
 	int ret;
+#endif
 
 	drm_mode_object_register(connector->dev, &connector->base);
 
@@ -3061,6 +3063,7 @@ int drm_mode_addfb(struct drm_device *dev,
 	struct drm_mode_fb_cmd *or = data;
 	static const struct drm_mode_fb_cmd2 zero_fbcmd;
 	struct drm_mode_fb_cmd2 r = zero_fbcmd;
+	int ret;
 
 	/* convert to new format and call new ioctl */
 	r.fb_id = or->fb_id;
@@ -3199,7 +3202,7 @@ static int framebuffer_check(const struct drm_mode_fb_cmd2 *r)
 		}
 
 		if (r->modifier[i] && !(r->flags & DRM_MODE_FB_MODIFIERS)) {
-			DRM_DEBUG_KMS("bad fb modifier %llu for plane %d\n",
+			DRM_DEBUG_KMS("bad fb modifier %"PRIu64" for plane %d\n",
 				      r->modifier[i], i);
 			return -EINVAL;
 		}
