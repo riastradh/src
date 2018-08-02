5d3d1b26d311d9d41c3449a9dcd017d7a309b363
Taylor R Campbell <riastradh@NetBSD.org>
2018-08-02 15:13:14 +0000

    enum pipe -> enum i915_pipe

--- sys/external/bsd/drm2/dist/drm/i915/intel_overlay.c
+++ sys/external/bsd/drm2/dist/drm/i915/intel_overlay.c
@@ -767,7 +767,7 @@ static int intel_overlay_do_put_image(struct intel_overlay *overlay,
 	bool scale_changed = false;
 	struct drm_device *dev __diagused = overlay->dev;
 	u32 swidth, swidthsw, sheight, ostride;
-	enum pipe pipe = overlay->crtc->pipe;
+	enum i915_pipe pipe = overlay->crtc->pipe;
 
 	WARN_ON(!mutex_is_locked(&dev->struct_mutex));
 	WARN_ON(!drm_modeset_is_locked(&dev->mode_config.connection_mutex));
