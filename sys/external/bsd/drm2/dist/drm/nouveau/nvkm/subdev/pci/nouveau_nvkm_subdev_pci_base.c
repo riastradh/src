be441e733c7cba89937b2aa31f2cb660f3799f01
Taylor R Campbell <riastradh@NetBSD.org>
2018-08-03 03:27:03 +0000

    Simplify with DRM_IRQ_ARGS, fix typo, ifdef out unnecessary stuff.

--- sys/external/bsd/drm2/dist/drm/nouveau/nvkm/subdev/pci/nouveau_nvkm_subdev_pci_base.c
+++ sys/external/bsd/drm2/dist/drm/nouveau/nvkm/subdev/pci/nouveau_nvkm_subdev_pci_base.c
@@ -70,13 +70,8 @@ nvkm_pci_rom_shadow(struct nvkm_pci *pci, bool shadow)
 	nvkm_pci_wr32(pci, 0x0050, data);
 }
 
-#ifdef __NetBSD__
-static int
-nvkm_pci_intr(void *arg)
-#else
 static irqreturn_t
-nvkm_pci_intr(int irq, void *arg)
-#endif
+nvkm_pci_intr(DRM_IRQ_ARGS)
 {
 	struct nvkm_pci *pci = arg;
 	struct nvkm_mc *mc = pci->subdev.device->mc;
@@ -149,7 +144,7 @@ nvkm_pci_init(struct nvkm_subdev *subdev)
 	const struct pci_attach_args *pa = &pdev->pd_pa;
 
 	/* XXX errno NetBSD->Linux */
-	ret = -pci_intr_alloc(pa, &pci->pci_ih, NULL, 0);
+	ret = -pci_intr_alloc(pa, &pci->pci_ihp, NULL, 0);
 	if (ret)
 		return ret;
 	pci->pci_intrcookie = pci_intr_establish_xname(pa->pa_pc,
@@ -162,9 +157,9 @@ nvkm_pci_init(struct nvkm_subdev *subdev)
 	ret = request_irq(pdev->irq, nvkm_pci_intr, IRQF_SHARED, "nvkm", pci);
 	if (ret)
 		return ret;
-#endif
 
 	pci->irq = pdev->irq;
+#endif
 
 	/* Ensure MSI interrupts are armed, for the case where there are
 	 * already interrupts pending (for whatever reason) at load time.
@@ -204,7 +199,9 @@ nvkm_pci_new_(const struct nvkm_pci_func *func, struct nvkm_device *device,
 	nvkm_subdev_ctor(&nvkm_pci_func, device, index, 0, &pci->subdev);
 	pci->func = func;
 	pci->pdev = device->func->pci(device)->pdev;
+#ifndef __NetBSD__
 	pci->irq = -1;
+#endif
 
 	if (device->type == NVKM_DEVICE_AGP)
 		nvkm_agp_ctor(pci);
