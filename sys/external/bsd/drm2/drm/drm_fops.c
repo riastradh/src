3259d138e654a434d72872aacfc1871a9fc24b93
Taylor R Campbell <riastradh@NetBSD.org>
2018-08-09 02:57:24 +0000

    Release the magic before releasing the master.
    Releasing the master frees the idr into which we are trying to
    release the magic.  Use of magic that has been freed sounds like a
    dangerous proposition.

--- sys/external/bsd/drm2/drm/drm_fops.c
+++ sys/external/bsd/drm2/drm/drm_fops.c
@@ -214,6 +214,12 @@ drm_close_file(struct drm_file *file)
 	struct drm_minor *const minor = file->minor;
 	struct drm_device *const dev = minor->dev;
 
+	mutex_lock(&dev->struct_mutex);
+	list_del(&file->lhead);
+	if (file->magic)
+		idr_remove(&file->master->magic_map, file->magic);
+	mutex_unlock(&dev->struct_mutex);
+
 	if (dev->driver->preclose)
 		(*dev->driver->preclose)(dev, file);
 
@@ -228,12 +234,6 @@ drm_close_file(struct drm_file *file)
 		drm_gem_release(dev, file);
 	drm_legacy_ctxbitmap_flush(dev, file);
 	drm_close_file_master(file);
-
-	mutex_lock(&dev->struct_mutex);
-	list_del(&file->lhead);
-	if (file->magic)
-		idr_remove(&file->master->magic_map, file->magic);
-	mutex_unlock(&dev->struct_mutex);
 
 	if (dev->driver->postclose)
 		(*dev->driver->postclose)(dev, file);
