8aa82fdd6e19efa7fe3e2899694fdb2c50e03f5c
Taylor R Campbell <riastradh@NetBSD.org>
2018-08-17 15:08:01 +0000

    Use kcopy, not memcpy, in case of failure to page in dst.  From chs@.

--- sys/external/bsd/drm2/dist/drm/i915/i915_cmd_parser.c
+++ sys/external/bsd/drm2/dist/drm/i915/i915_cmd_parser.c
@@ -983,7 +983,15 @@ static u32 *copy_batch(struct drm_i915_gem_object *dest_obj,
 	if (needs_clflush)
 		drm_clflush_virt_range(src, batch_len);
 
+#ifdef __NetBSD__
+	ret = -kcopy(dst, src, batch_len);
+	if (ret) {
+		uvm_unmap(kernel_map, dstva, dstva + dstlen);
+		goto unmap_src;
+	}
+#else
 	memcpy(dst, src, batch_len);
+#endif
 
 unmap_src:
 #ifdef __NetBSD__
