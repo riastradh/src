7cef1f41a4f61675aabdac0cc32192a1c86ae4a2
Taylor R Campbell <riastradh@NetBSD.org>
2018-09-09 16:04:10 +0000

    Just a bare $NetBSD$, not its expansion for prepare-import.sh.

--- sys/external/bsd/drm2/prepare-import.sh
+++ sys/external/bsd/drm2/prepare-import.sh
@@ -13,7 +13,7 @@ set -Ceu
 find . -name '*.h' \
 | while read f; do
 	cleantags "$f"
-	(printf '/*\t$NetBSD: prepare-import.sh,v 1.1 2018/08/27 00:46:21 riastradh Exp $\t*/\n\n' && cat -- "$f") > "$f".tmp
+	(printf '/*\t%c%s%c\t*/\n\n' '$' NetBSD '$' && cat -- "$f") > "$f".tmp
 	mv -f -- "$f".tmp "$f"
 done
 
