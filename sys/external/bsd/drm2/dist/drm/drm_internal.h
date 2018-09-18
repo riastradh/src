d6069db4d3e59bf5ecd775a8fdc32802ee8d5cb5
Taylor R Campbell <riastradh@NetBSD.org>
2018-09-18 15:05:41 +0000

    Declare and initialize drm_unplug_srcu our way.

--- sys/external/bsd/drm2/dist/drm/drm_internal.h
+++ sys/external/bsd/drm2/dist/drm/drm_internal.h
@@ -90,6 +90,8 @@ void drm_prime_remove_buf_handle_locked(struct drm_prime_file_private *prime_fpr
 struct drm_minor *drm_minor_acquire(unsigned int minor_id);
 void drm_minor_release(struct drm_minor *minor);
 #ifdef __NetBSD__
+#include <linux/srcu.h>
+extern struct srcu drm_unplug_srcu;
 extern struct spinlock drm_minor_lock;
 extern struct idr drm_minors_idr;
 #endif
