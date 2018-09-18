4c6c4402840da13b1ce48df2fd97842531504eaa
Taylor R Campbell <riastradh@NetBSD.org>
2018-09-18 14:29:46 +0000

    Add some necessry header files.

--- sys/external/bsd/drm2/dist/include/drm/drm_dp_helper.h
+++ sys/external/bsd/drm2/dist/include/drm/drm_dp_helper.h
@@ -28,6 +28,7 @@
 #include <linux/delay.h>
 #include <linux/i2c.h>
 #include <linux/types.h>
+#include <linux/workqueue.h>
 
 /*
  * Unless otherwise noted, all values are from the DP 1.1a spec.  Note that
