aaed1f94e116832741ffae8fc93af4fcc13200b1
Taylor R Campbell <riastradh@NetBSD.org>
2018-10-13 07:16:23 +0000

    Define empty __deprecated.

--- sys/external/bsd/common/include/linux/kernel.h
+++ sys/external/bsd/common/include/linux/kernel.h
@@ -74,6 +74,7 @@
 #endif
 #define	__always_unused	__unused
 #define	noinline	__noinline
+#define	__deprecated	/* nothing */
 
 #define	barrier()	__insn_barrier()
 #define	likely(X)	__predict_true(X)
