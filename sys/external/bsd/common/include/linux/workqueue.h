aeb703d7c475593bdc28393d722f8b969463bba7
Taylor R Campbell <riastradh@NetBSD.org>
2018-10-14 21:52:24 +0000

    Stubs for init/destroy delayed work on stack.

--- sys/external/bsd/common/include/linux/workqueue.h
+++ sys/external/bsd/common/include/linux/workqueue.h
@@ -133,6 +133,8 @@ struct work_struct *
 	current_work(void);
 
 #define	INIT_WORK_ONSTACK		INIT_WORK
+#define	INIT_DELAYED_WORK_ONSTACK	INIT_DELAYED_WORK
+#define	destroy_delayed_work_on_stack(dw)	__nothing
 
 static inline void
 destroy_work_on_stack(struct work_struct *work)
