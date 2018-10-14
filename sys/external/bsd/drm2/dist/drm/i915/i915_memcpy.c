b24c00fa329af32646af3cd34e3e143e0542e578
Taylor R Campbell <riastradh@NetBSD.org>
2018-10-14 21:31:58 +0000

    No need to port this until we deal with movntdqa (and kernel FPU use).

--- sys/external/bsd/drm2/dist/drm/i915/i915_memcpy.c
+++ sys/external/bsd/drm2/dist/drm/i915/i915_memcpy.c
@@ -101,6 +101,7 @@ bool i915_memcpy_from_wc(void *dst, const void *src, unsigned long len)
 
 void i915_memcpy_init_early(struct drm_i915_private *dev_priv)
 {
+#ifdef CONFIG_AS_MOVNTDQA
 	/*
 	 * Some hypervisors (e.g. KVM) don't support VEX-prefix instructions
 	 * emulation. So don't enable movntdqa in hypervisor guest.
@@ -108,4 +109,5 @@ void i915_memcpy_init_early(struct drm_i915_private *dev_priv)
 	if (static_cpu_has(X86_FEATURE_XMM4_1) &&
 	    !boot_cpu_has(X86_FEATURE_HYPERVISOR))
 		static_branch_enable(&has_movntdqa);
+#endif
 }
