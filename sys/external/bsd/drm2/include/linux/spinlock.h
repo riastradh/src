3cfe91f457fd6eb3e3ecab38ddd971ddbc810e1e
Taylor R Campbell <riastradh@NetBSD.org>
2018-10-20 03:25:12 +0000

    spin_lock_nested

--- sys/external/bsd/drm2/include/linux/spinlock.h
+++ sys/external/bsd/drm2/include/linux/spinlock.h
@@ -89,6 +89,12 @@ spin_unlock_irqrestore(spinlock_t *spinlock, unsigned long __unused flags)
 }
 
 static inline void
+spin_lock_nested(spinlock_t *spinlock, int subclass)
+{
+	spin_lock(spinlock);
+}
+
+static inline void
 spin_lock_init(spinlock_t *spinlock)
 {
 	/* XXX What's the right IPL?  IPL_DRM...?  */
