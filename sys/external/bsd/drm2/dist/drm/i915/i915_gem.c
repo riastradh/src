27b7af6bf9bf08f319e31e4143472704288c1537
Maya Rashish <maya@NetBSD.org>
2020-03-13 23:53:29 +0200

    Don't ifdef __NetBSD__ around ALIGN; just patch to round_up.

--- sys/external/bsd/drm2/dist/drm/i915/i915_gem.c
+++ sys/external/bsd/drm2/dist/drm/i915/i915_gem.c
@@ -283,20 +283,12 @@ i915_gem_dumb_create(struct drm_file *file,
 	}
 
 	/* have to work out size/pitch and return them */
-#ifdef __NetBSD__		/* ALIGN means something else. */
 	args->pitch = round_up(args->width * cpp, 64);
-#else
-	args->pitch = ALIGN(args->width * cpp, 64);
-#endif
 
 	/* align stride to page size so that we can remap */
 	if (args->pitch > intel_plane_fb_max_stride(to_i915(dev), format,
 						    DRM_FORMAT_MOD_LINEAR))
-#ifdef __NetBSD__               /* ALIGN means something else.  */
 		args->pitch = round_up(args->pitch, 4096);
-#else
-		args->pitch = ALIGN(args->pitch, 4096);
-#endif
 
 	if (args->pitch < args->width)
 		return -EINVAL;
