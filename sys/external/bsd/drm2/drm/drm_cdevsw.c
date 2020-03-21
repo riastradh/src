b38c867f818acbc487ffbac1acfe55b46ffc0db6
Maya Rashish <maya@NetBSD.org>
2020-03-21 19:15:33 +0200

    Include some headers now that drmP.h got split off.

--- sys/external/bsd/drm2/drm/drm_cdevsw.c
+++ sys/external/bsd/drm2/drm/drm_cdevsw.c
@@ -58,6 +58,9 @@ __KERNEL_RCSID(0, "$NetBSD: drm_cdevsw.c,v 1.15 2020/12/19 22:09:15 thorpej Exp 
 #include <linux/pm.h>
 
 #include <drm/drmP.h>
+#include <drm/drm_drv.h>
+#include <drm/drm_file.h>
+#include <drm/drm_legacy.h>
 
 #include "../dist/drm/drm_internal.h"
 #include "../dist/drm/drm_legacy.h"
