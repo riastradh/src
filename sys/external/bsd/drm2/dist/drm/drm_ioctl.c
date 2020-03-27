258b9ca395d46022b4cff5514630c24ab10d214d
Maya Rashish <maya@NetBSD.org>
2020-03-27 16:11:36 +0300

    Port DRM_ATOMIC disabling code

--- sys/external/bsd/drm2/dist/drm/drm_ioctl.c
+++ sys/external/bsd/drm2/dist/drm/drm_ioctl.c
@@ -353,7 +353,11 @@ drm_setclientcap(struct drm_device *dev, void *data, struct drm_file *file_priv)
 		if (!drm_core_check_feature(dev, DRIVER_ATOMIC))
 			return -EOPNOTSUPP;
 		/* The modesetting DDX has a totally broken idea of atomic. */
+#ifdef __linux__
 		if (current->comm[0] == 'X' && req->value == 1) {
+#else
+		if (current->p_comm[0] == 'X' && req->value == 1) {
+#endif
 			pr_info("broken atomic modeset userspace detected, disabling atomic\n");
 			return -EOPNOTSUPP;
 		}
