129be690eaac64c9789368dc79dc8d3bbac2721c
Maya Rashish <maya@NetBSD.org>
2020-03-27 16:12:54 +0300

    Provide our own declaration of drm_irq_install

--- sys/external/bsd/drm2/dist/include/drm/drm_irq.h
+++ sys/external/bsd/drm2/dist/include/drm/drm_irq.h
@@ -28,7 +28,12 @@
 
 struct drm_device;
 
+#ifdef __NetBSD__
+int drm_irq_install(struct drm_device *dev);
+#else
 int drm_irq_install(struct drm_device *dev, int irq);
+#endif
+
 int drm_irq_uninstall(struct drm_device *dev);
 
 #endif
