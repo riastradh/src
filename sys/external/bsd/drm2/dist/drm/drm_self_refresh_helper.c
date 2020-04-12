127782e907fd63c436b81448940d59d269252526
Taylor R Campbell <riastradh@NetBSD.org>
2020-04-12 18:18:59 +0000

    Fix typo.

--- sys/external/bsd/drm2/dist/drm/drm_self_refresh_helper.c
+++ sys/external/bsd/drm2/dist/drm/drm_self_refresh_helper.c
@@ -283,7 +283,7 @@ void drm_self_refresh_helper_cleanup(struct drm_crtc *crtc)
 	crtc->self_refresh_data = NULL;
 
 	cancel_delayed_work_sync(&sr_data->entry_work);
-	mutex_destroy(&sr_data->svg_mutex);
+	mutex_destroy(&sr_data->avg_mutex);
 	kfree(sr_data);
 }
 EXPORT_SYMBOL(drm_self_refresh_helper_cleanup);
