0bb53c5d265484242491ee930e1e875cbf135418
Taylor R Campbell <riastradh@NetBSD.org>
2020-10-05 20:32:03 +0000

    radeon: Fix missing unlocks in radeon fence.

--- sys/external/bsd/drm2/dist/drm/radeon/radeon_fence.c
+++ sys/external/bsd/drm2/dist/drm/radeon/radeon_fence.c
@@ -326,6 +326,9 @@ static void radeon_fence_check_lockup(struct work_struct *work)
 	if (!down_read_trylock(&rdev->exclusive_lock)) {
 		/* just reschedule the check if a reset is going on */
 		radeon_fence_schedule_check(rdev, ring);
+#ifdef __NetBSD__
+		spin_unlock(&rdev->fence_lock);
+#endif
 		return;
 	}
 
@@ -524,6 +527,7 @@ bool radeon_fence_signaled(struct radeon_fence *fence)
 		ret = dma_fence_signal_locked(&fence->base);
 		if (!ret)
 			DMA_FENCE_TRACE(&fence->base, "signaled from radeon_fence_signaled\n");
+		spin_unlock(&fence->rdev->fence_lock);
 		return true;
 	}
 	spin_unlock(&fence->rdev->fence_lock);
