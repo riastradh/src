a924aa232f5cf542656cfa6ec0f4306c65816f61
Taylor R Campbell <riastradh@NetBSD.org>
2021-01-01 01:29:56 +0000

    drm/dma-fence: Fix zero timeout semantics in dma_fence_default_wait.

--- sys/external/bsd/drm2/linux/linux_dma_fence.c
+++ sys/external/bsd/drm2/linux/linux_dma_fence.c
@@ -782,10 +782,11 @@ dma_fence_wait(struct dma_fence *fence, bool intr)
  *
  *	Default implementation of fence wait callback using a condition
  *	variable.  If the fence is already signalled, return timeout,
- *	or 1 if no timeout.  If the enable signalling callback hasn't
- *	been called, call it, and if it fails, act as if the fence had
- *	been signalled.  Otherwise, wait on the internal condvar.  If
- *	timeout is MAX_SCHEDULE_TIMEOUT, treat it as no timeout.
+ *	or 1 if timeout is zero meaning poll.  If the enable signalling
+ *	callback hasn't been called, call it, and if it fails, act as
+ *	if the fence had been signalled.  Otherwise, wait on the
+ *	internal condvar.  If timeout is MAX_SCHEDULE_TIMEOUT, wait
+ *	indefinitely.
  */
 long
 dma_fence_default_wait(struct dma_fence *fence, bool intr, long timeout)
@@ -800,15 +801,20 @@ dma_fence_default_wait(struct dma_fence *fence, bool intr, long timeout)
 
 	/* Optimistically try to skip the lock if it's already signalled.  */
 	if (fence->flags & (1u << DMA_FENCE_FLAG_SIGNALED_BIT))
-		return (timeout < MAX_SCHEDULE_TIMEOUT ? timeout : 1);
+		return (timeout ? timeout : 1);
 
 	/* Acquire the lock.  */
 	spin_lock(fence->lock);
 
-	/* Ensure signalling is enabled, or fail if we can't.  */
-	ret = dma_fence_ensure_signal_enabled(fence);
-	if (ret)
-		goto out;
+	/* Ensure signalling is enabled, or stop if already completed.  */
+	if (dma_fence_ensure_signal_enabled(fence) != 0)
+		return (timeout ? timeout : 1);
+
+	/* If merely polling, stop here.  */
+	if (timeout == 0) {
+		spin_unlock(fence->lock);
+		return 0;
+	}
 
 	/* Find out what our deadline is so we can handle spurious wakeup.  */
 	if (timeout < MAX_SCHEDULE_TIMEOUT) {
@@ -854,7 +860,6 @@ dma_fence_default_wait(struct dma_fence *fence, bool intr, long timeout)
 		}
 	}
 
-out:
 	/* All done.  Release the lock.  */
 	spin_unlock(fence->lock);
 
