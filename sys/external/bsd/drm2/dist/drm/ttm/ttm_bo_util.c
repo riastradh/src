86ebe6c606df1298fa305182719ddcc7bc0c8628
Maya Rashish <maya@NetBSD.org>
2021-01-02 10:11:25 +0200

    drm/ttm: Conditionalize initialization of vma node.
    Needs to match conditional destruction.

--- sys/external/bsd/drm2/dist/drm/ttm/ttm_bo_util.c
+++ sys/external/bsd/drm2/dist/drm/ttm/ttm_bo_util.c
@@ -567,7 +567,8 @@ static int ttm_buffer_object_transfer(struct ttm_buffer_object *bo,
 	INIT_LIST_HEAD(&fbo->base.io_reserve_lru);
 	fbo->base.moving = NULL;
 #ifdef __NetBSD__
-	drm_vma_node_init(&fbo->base.base.vma_node);
+	if (!ttm_bo_uses_embedded_gem_object(bo))
+		drm_vma_node_init(&fbo->base.base.vma_node);
 	uvm_obj_init(&fbo->base.uvmobj, bo->bdev->driver->ttm_uvm_ops, true, 1);
 	rw_obj_hold(bo->uvmobj.vmobjlock);
 	uvm_obj_setlock(&fbo->base.uvmobj, bo->uvmobj.vmobjlock);
