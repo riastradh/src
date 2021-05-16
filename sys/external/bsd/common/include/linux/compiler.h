1c794f38354e14bd63ea31aa7ae7ab889369d849
Taylor R Campbell <riastradh@NetBSD.org>
2021-05-16 09:37:09 +0000

    linux: Add __same_type and __must_be_array.

--- sys/external/bsd/common/include/linux/compiler.h
+++ sys/external/bsd/common/include/linux/compiler.h
@@ -54,6 +54,10 @@
 #define	barrier()	__insn_barrier()
 #define	likely(X)	__predict_true(X)
 #define	unlikely(X)	__predict_false(X)
+#define	__same_type(X,Y)						      \
+	__builtin_types_compatible_p(__typeof__(X), __typeof__(Y))
+#define	__must_be_array(X)						      \
+	BUILD_BUG_ON_ZERO(__same_type((X), &(X)[0]))
 
 #define	READ_ONCE(X)	({						      \
 	typeof(X) __read_once_tmp = (X);				      \
