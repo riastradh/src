447d59790a5362f8d19f2585b37313d3bc60051c
Taylor R Campbell <riastradh@NetBSD.org>
2021-05-16 09:40:08 +0000

    drm/i915: Unhook i915_gem_client_blt.c and revert changes.
    Only used by self-tests, which we're not using (for now...).

--- sys/external/bsd/drm2/i915drm/i915_module.c
+++ sys/external/bsd/drm2/i915drm/i915_module.c
@@ -53,8 +53,6 @@ MODULE(MODULE_CLASS_DRIVER, i915drmkms, "drmkms,drmkms_pci"); /* XXX drmkms_i2c 
 struct drm_sysctl_def i915_def = DRM_SYSCTL_INIT();
 
 int i915_global_buddy_init(void); /* XXX */
-void i915_gem_client_blt_init(void); /* XXX */
-void i915_gem_client_blt_fini(void); /* XXX */
 
 static int
 i915drmkms_init(void)
@@ -71,7 +69,6 @@ i915drmkms_init(void)
 
 	drm_sysctl_init(&i915_def);
 	spin_lock_init(&mchdev_lock);
-	i915_gem_client_blt_init();
 
 	return 0;
 }
@@ -93,7 +90,6 @@ static void
 i915drmkms_fini(void)
 {
 
-	i915_gem_client_blt_fini();
 	spin_lock_destroy(&mchdev_lock);
 	drm_sysctl_fini(&i915_def);
 }
