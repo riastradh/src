b2039a5bbf299072297efceb78f117d7f351a7b1
Taylor R Campbell <riastradh@NetBSD.org>
2021-05-30 22:03:05 +0000

    linux: S16_MAX, S32_MAX, S64_MAX

--- sys/external/bsd/common/include/linux/kernel.h
+++ sys/external/bsd/common/include/linux/kernel.h
@@ -52,6 +52,10 @@
 #define U32_MAX UINT32_MAX
 #define U64_MAX UINT64_MAX
 
+#define	S16_MAX	INT16_MAX
+#define	S32_MAX	INT32_MAX
+#define	S64_MAX	INT64_MAX
+
 #define	oops_in_progress	(panicstr != NULL)
 
 #define	IS_BUILTIN(option)	(1) /* Probably... */
