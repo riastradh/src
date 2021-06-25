dca21e23def2e539ecb854dcd31605c46b598e8c
Taylor R Campbell <riastradh@NetBSD.org>
2021-06-25 22:00:54 +0000

    drm: Restore drm_crtc_vblank_put_locked

--- sys/external/bsd/drm2/dist/include/drm/drm_vblank.h
+++ sys/external/bsd/drm2/dist/include/drm/drm_vblank.h
@@ -236,6 +236,7 @@ bool drm_handle_vblank(struct drm_device *dev, unsigned int pipe);
 bool drm_crtc_handle_vblank(struct drm_crtc *crtc);
 int drm_crtc_vblank_get(struct drm_crtc *crtc);
 void drm_crtc_vblank_put(struct drm_crtc *crtc);
+void drm_crtc_vblank_put_locked(struct drm_crtc *crtc);
 void drm_wait_one_vblank(struct drm_device *dev, unsigned int pipe);
 void drm_crtc_wait_one_vblank(struct drm_crtc *crtc);
 void drm_crtc_vblank_off(struct drm_crtc *crtc);
