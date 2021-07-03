c0b31d8d86e3b009ef4ffc8e59cbe434b4a4b801
Taylor R Campbell <riastradh@NetBSD.org>
2021-07-03 18:28:21 +0000

    i915: Missed a spot in flailing the bad thing going wrong.

--- sys/external/bsd/drm2/dist/drm/i915/i915_active.c
+++ sys/external/bsd/drm2/dist/drm/i915/i915_active.c
@@ -939,7 +939,7 @@ __i915_active_fence_set(struct i915_active_fence *active,
 	KASSERT(active->cb.func == node_retire ||
 	    active->cb.func == excl_retire ||
 	    active->cb.func == i915_active_noop);
-	dma_fence_add_callback(fence, &active->cb, node_retire);
+	dma_fence_add_callback(fence, &active->cb, active->cb.func);
 #endif
 
 	return prev;
