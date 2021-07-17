50097d130c7fef46ab0e55b6c3a92b549dffdaef
Taylor R Campbell <riastradh@NetBSD.org>
2021-07-17 19:31:14 +0000

    Revert "WIP: debug console boot"
    This reverts commit f8699fb43533eea0710114a21934ce6cf77b3da3.

--- sys/arch/i386/stand/lib/pcio.c
+++ sys/arch/i386/stand/lib/pcio.c
@@ -87,10 +87,8 @@ getcomaddr(int idx)
 {
 	short addr;
 #ifdef CONSADDR
-	if (CONSADDR != 0) {
-		printf("%s: CONSADDR=0x%x\n", __func__, CONSADDR);
+	if (CONSADDR != 0)
 		return CONSADDR;
-	}
 #endif
 	/* read in BIOS data area */
 	pvbcopy((void *)(0x400 + 2 * idx), &addr, 2);
@@ -167,10 +165,8 @@ ok:
 	case CONSDEV_COM1:
 	case CONSDEV_COM2:
 	case CONSDEV_COM3:
-		printf("set iodev=0x%x\n", dev);
 		iodev = dev;
 		btinfo_console.addr = getcomaddr(iodev - CONSDEV_COM0);
-		printf("set consaddr=0x%x\n", btinfo_console.addr);
 		if (!btinfo_console.addr)
 			goto nocom;
 		cominit_x();
